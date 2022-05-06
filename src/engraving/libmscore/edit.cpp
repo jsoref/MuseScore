92109e1e6535125244f4f6bc0cbb71aa8bf67580
Josh Soref <jsoref@users.noreply.github.com>
2022-05-06 17:35:21 -0400

    spelling: signatures
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/engraving/libmscore/edit.cpp
+++ src/engraving/libmscore/edit.cpp
@@ -3734,7 +3734,7 @@ MeasureBase* Score::insertMeasure(ElementType type, MeasureBase* beforeMeasure, 
             }
 
             //
-            // move clef, time, key signatrues
+            // move clef, time, key signatures
             //
             for (TimeSig* ts : tsl) {
                 TimeSig* nts = Factory::copyTimeSig(*ts);
