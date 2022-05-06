caf79d3bc4603896f2589380e55f80789529c77e
Josh Soref <jsoref@users.noreply.github.com>
2022-05-06 17:31:17 -0400

    spelling: layed out
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/engraving/libmscore/note.cpp
+++ src/engraving/libmscore/note.cpp
@@ -2190,7 +2190,7 @@ void Note::layout()
         if (_ghost) {
             parenthesis = true;
         }
-        // not complete but we need systems to be layouted to add parenthesis
+        // not complete but we need systems to be layed out to add parenthesis
         if (_fixed) {
             _fretString = "/";
         } else {
